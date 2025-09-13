414a93c7ef92767fa6eeaecff3777e4d9303a13b
Poula17 <139756038+Poula17@users.noreply.github.com>
2025-09-13 23:05:43 +0300

    Update Education.jsx

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,7 +1,7 @@
 function Education() {
   return (
-    <div>
-      <h2 className="text-xl font-bold mb-4">Education</h2>
+    <div className="flex flex-col items-center text-center space-y-2">
+      <h2 className="text-2xl font-bold text-purple-700">Education</h2>
       <p>Bachelor of Computer Engineering and Science</p>
       <p>Faculty of Electronic Engineering, Menouf</p>
       <p>Expected Graduation: 2026</p>
